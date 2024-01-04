924b600a0101ff42f74ea999a22ca5cba492bd34
Mark <78240560+xyzuser12@users.noreply.github.com>
2024-01-04 22:41:39 +0800

    feat; updated user backend

--- backend/db.php
+++ backend/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ebp";
+$dbname = "ebarangay_portal";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
